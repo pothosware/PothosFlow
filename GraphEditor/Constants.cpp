2decbcfcd49a9b30d808ba068d86f3db1c3976a9
Josh Blum <josh@joshknows.com>
2021-07-11 17:02:44 -0500

    missing source from prior commit

---
+++ GraphEditor/Constants.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2021-2021 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#include "GraphEditor/Constants.hpp"
+#include "ColorUtils/ColorUtils.hpp"
+#include <QLabel>
+#include <iostream>
+
+QString defaultPaletteBackground(void)
+{
+    QLabel label;
+    auto color = label.palette().color(QPalette::Background);
+    return color.name();
+}
+
+static bool isDarkMode(void)
+{
+    const static bool isDark = isColorDark(defaultPaletteBackground());
+    return isDark;
+}
+
+QString darkColorSupport(const QString &light, const QString &dark)
+{
+    return isDarkMode()?dark:light;
+}
